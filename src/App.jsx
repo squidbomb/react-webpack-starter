0932f5a2f10fe5a9650e350892d5160f9bbcae34
Anthony De Jesus <adejesus@worldvision.ph>
2020-05-07 09:53:43 +0800

    updated dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 // import { hot } from "react-hot-loader";
 import "./App.css";
+import logo from "./assets/logo192.png";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="App" style={{textAlign: "center"}}>
+                <img src={logo} alt="" />
                 <h1> Hello, World!!! </h1>
             </div>
         );
